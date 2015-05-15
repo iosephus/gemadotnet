51dec11da61abf1ae1e0bdf657822479eda5735c
Jose M. Perez-Sanchez <me@josemperez.com>
2015-05-14 22:51:19 -0400

    Remove spaces around values in vector printing

--- Gema/gema.fs
+++ Gema/gema.fs
@@ -13,5 +13,5 @@ let main argv =
     let results = run Options
     printfn "This is the .NET implementation of Gema version 0.1"
     printfn "Running with NumberOfParticles = %d, NumberOfSteps = %d" Options.NumberOfParticles Options.NumberOfSteps
-    printfn "Results = { Step = %d; State = %d; Position = [| %f; %f, %f |]; }" results.Step results.State results.Position.[0] results.Position.[1] results.Position.[2]
+    printfn "Results = { Step = %d; State = %d; Position = [|%f; %f, %f|]; }" results.Step results.State results.Position.[0] results.Position.[1] results.Position.[2]
     0 // return an integer exit code
