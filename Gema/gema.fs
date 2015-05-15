9f767b0480a2b52bcac959818493996b6e24fc3f
Jose M. Perez-Sanchez <me@josemperez.com>
2015-05-15 10:27:47 -0400

    Create functions for start/next point

--- Gema/gema.fs
+++ Gema/gema.fs
@@ -6,20 +6,28 @@ open MathNet.Numerics.Random
 type Config = { NumberOfParticles: int; NumberOfSteps: int; StorageInterval: int; }
 type Point = { Step: int; State: int; Position: float array; }
 
+let GetStartPpoint (options:Config) (randomGenerator:MersenneTwister) =
+    let startX = randomGenerator.NextDouble()
+    let startY = randomGenerator.NextDouble()
+    let startZ = randomGenerator.NextDouble()
+    { Step = 0;  State = 0; Position = [|startX; startY; startZ|]; }
+
+let GetNextPoint (options:Config) (prevPoint:Point) (randomGenerator:MersenneTwister) =
+    let newX = prevPoint.Position.[0] + randomGenerator.NextDouble()
+    let newY = prevPoint.Position.[1] + randomGenerator.NextDouble()
+    let newZ = prevPoint.Position.[2] + randomGenerator.NextDouble()
+    { Step = prevPoint.Step + 1;  State = 0; Position = [|newX; newY; newZ|]; }
 
 let WalkOneParticle (options:Config) (randomGenerator:MersenneTwister) =
     let rec _walkOneParticle (acc:List<Point>) (prevPoint:Point) (stepsToDo:int) (randomGenerator:MersenneTwister) =
         match stepsToDo with
         | 0 -> acc
         | todo ->
-            let newX = prevPoint.Position.[0] + randomGenerator.NextDouble()
-            let newY = prevPoint.Position.[1] + randomGenerator.NextDouble()
-            let newZ = prevPoint.Position.[2] + randomGenerator.NextDouble()
-            let newPoint = { Step = prevPoint.Step + 1;  State = 0; Position = [|newX; newY; newZ|]; }
+            let newPoint = GetNextPoint options prevPoint randomGenerator
             let newAcc = match newPoint.Step % options.StorageInterval with | 0 -> (List.append acc [newPoint]) | _ -> acc
             _walkOneParticle newAcc newPoint (todo - 1) randomGenerator
-    let initPoint = { Step = 0;  State = 0; Position = [|0.0; 0.0; 0.0|]; }
-    _walkOneParticle [initPoint] initPoint options.NumberOfSteps randomGenerator
+    let startPoint = GetStartPpoint options randomGenerator
+    _walkOneParticle [startPoint] startPoint options.NumberOfSteps randomGenerator
 
 let run config =
     let randomGenerator = new MersenneTwister(93849329 )
@@ -32,7 +40,7 @@ let run config =
 
 [<EntryPoint>]
 let main argv = 
-    let Options = { NumberOfParticles = 10000; NumberOfSteps = 100000000; StorageInterval = 100000000; }
+    let Options = { NumberOfParticles = 100000; NumberOfSteps = 100000; StorageInterval = 1000; }
     let results = run Options
     printfn "This is the .NET implementation of Gema version 0.1"
     printfn "Running with NumberOfParticles = %d, NumberOfSteps = %d; StorageInterval = %d" Options.NumberOfParticles Options.NumberOfSteps Options.StorageInterval
