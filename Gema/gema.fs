a9f6b9a77c6c84c18a7e6b81144138e74d74c751
Jose M. Perez-Sanchez <me@josemperez.com>
2015-05-14 22:49:30 -0400

    Make some small changes to get familiar with the environment

--- Gema/gema.fs
+++ Gema/gema.fs
@@ -1,7 +1,17 @@
 ﻿// Learn more about F# at http://fsharp.net
 // See the 'F# Tutorial' project for more help.
 
+type Config = { NumberOfParticles: int; NumberOfSteps: int; }
+type Point = { Step: int; State: int; Position: float array; }
+
+let run config =
+    { Step = 0;  State = 0; Position = [|0.0; 0.0; 0.0|]; }
+
 [<EntryPoint>]
 let main argv = 
-    printfn "%A" argv
+    let Options = { NumberOfParticles = 1000; NumberOfSteps = 1000; }
+    let results = run Options
+    printfn "This is the .NET implementation of Gema version 0.1"
+    printfn "Running with NumberOfParticles = %d, NumberOfSteps = %d" Options.NumberOfParticles Options.NumberOfSteps
+    printfn "Results = { Step = %d; State = %d; Position = [| %f; %f, %f |]; }" results.Step results.State results.Position.[0] results.Position.[1] results.Position.[2]
     0 // return an integer exit code
