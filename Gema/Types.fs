8d5c7542d44238feb26a49f7ab6a0f7a51ad6329
Jose M. Perez-Sanchez <me@josemperez.com>
2015-05-16 18:31:48 -0400

    Add accidentally untracker file

---
+++ Gema/Types.fs
@@ -0,0 +1,23 @@
+﻿
+namespace Gema
+
+open System
+open MathNet.Numerics.Distributions
+
+type SimulationInfo = { NumberOfParticles: int;
+                            NumberOfSteps: int;
+                            StorageInterval: int;
+                            StepSize: float; }
+
+type ModelPars = { r: float; }
+
+type Point = { Step: int; State: int; Position: float array; }
+
+type ModelPDFs = { startPDF: IContinuousDistribution; stepPDF: IContinuousDistribution; randomGenerator: Random; }
+
+type IModelInfo =
+    abstract member Name: string
+
+type IGemaModel =
+    abstract member CreateModelInfo: ModelPars -> IModelInfo
+    abstract member CreatePDFs: SimulationInfo -> IModelInfo -> int -> ModelPDFs
