caff3aa57cba941de807c481f5bdcc6bf1d2d871
Jose M. Perez-Sanchez <me@josemperez.com>
2015-05-17 21:55:56 -0400

    Add untracked files from new project forgotten in previous commit

---
+++ Gema.Common/Types.fs
@@ -0,0 +1,30 @@
+﻿
+namespace Gema
+
+open System
+open MathNet.Numerics.Distributions
+
+type SimulationParameters = { NumberOfParticles: int;
+                        NumberOfSteps: int;
+                        StorageInterval: int;
+                        StepSize: float; }
+
+type Point = { Step: int; State: int; Position: float array; }
+
+type ModelPDFs = { StartPDF: IContinuousDistribution; StepPDF: IContinuousDistribution; RandomGenerator: Random; }
+
+type ModelFunctions = { GetStartPosition: unit -> float array;
+                        CheckStartPosition: float array -> int option;
+                        GetDisplacement: Point -> float array;
+                        CheckDisplacement: Point -> float array -> int option;
+                       }
+
+type IGemaModelInputParameters = interface end
+
+type IGemaModelParameters = interface end
+
+type IGemaModel =
+    abstract member GetStartPosition: unit -> float array;
+    abstract member CheckStartPosition: float array -> int option;
+    abstract member GetDisplacement: Point -> float array;
+    abstract member CheckDisplacement: Point -> float array -> int option;
